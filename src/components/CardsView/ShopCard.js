03709ff15a085d473d1df48225306965ca2e4290
DAB <dab62@yandex.ru>
2022-12-12 23:24:09 +0300

    layout

---
+++ src/components/CardsView/ShopCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './cards.css';
+
+function ShopCard({card, key}) {
+  console.log('ShopCard ');
+  return (
+    <div className='shopCard'>
+      <h2 className='heading'>{card.name}</h2>
+      <p className='coloritem'>{card.color}</p>
+      <img src={card.img} alt='' />
+      <div className='bodyCart'>
+        <span className='bodyCart_price'>${card.price}</span>
+        <button className='buttonCart'>Add to Cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default ShopCard;
